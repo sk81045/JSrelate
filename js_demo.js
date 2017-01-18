eddc80e434bfa8bbe477fd596ae3e9bcfb4ee4e0
cult huang <sk81045@126.com>
2017-01-18 16:37:57 +0800

    1/18

--- js_demo.js
+++ js_demo.js
@@ -1,136 +1,28 @@
 
 
-// alert("我的第一个 JavaScript");
-// document.getElementById("demo").innerHTML="修改。";  获取标签ID 替换内容
-
-function myfun(num){
-	alert(num);
-	document.write(num);
-	console.log(num); 
-}
-function inner(num){  
-	document.getElementById("pp").innerHTML = num;
-}
-
-function anony(){    //arguments是默认参数指针
-	alert(arguments[0]);
-}
-
-function factorial(num){    //递归函数 arguments.callee是默认的当前执行函数指针
-	if(num <= 1){
-		return 5;
-	}else{
-		return num * arguments.callee(num-1);
-	}
-}
-
-var person = {
-	name:'小明',
-	age:'18岁',
-	sex:'男',
-	hobby:'摇滚乐'
-};
-
-var table = ['red','blue','yellow'];
-table.push('green');
-var item = table.pop();
-
-function callSomeFunction(someFunction,someArgument){
-	alert(someFunction(someArgument));
-}
-
-function add10(num){
-	return num + 10;
-}
- // callSomeFunction(add10,10);
-
-function factorial(num){  //简易递归程序  (3+2+1)
-	if(num <= 1){ 
-		return 1;
-	}else{
-		return num + arguments.callee(num-1);
-	}
-}
-var test = factorial(3);
-
-// alert(test);
-function sumx(num1,num2){
-	return num1 + num2;
-}
-function callSum1(num1,num2){
-	return sumx.apply(this,arguments);
-}
-function callSum2(num1,num2){
-	return sumx.apply(this,[num1,num2]);
-}
-// alert(callSum1(10,10));
-// alert(callSum2(10,10));
-
-// window.color = "red";
-// var o = {color:"blue"};
-// function sayColor(){
-// 	alert(this.color);
-// }
-// sayColor.call(o);
-
-
-eval("function say(num){ alert(num) }");
-// say('fuck world');
-
-
-
-var arr = [1,2,3,34,56,566,3,23,45,57];
-var arrMax = Math.min.apply(Math,arr);
-// alert(arrMax); 
-
-var person = {};
-Object.defineProperty(person,"name",{
-	writable: false,
-	value: "HXB"
-});
-
-// alert(person.name);
-person.name = "Greg";
-// alert(person.name);
-
-function createPerson(name,age,job){   //工厂模式
-   var o = new Object();
-   o.name = name;
-   o.age  = age;
-   o.job  = job;
-   o.sqyName = function(){
-   	 alert(this.name);
-   };
-   return o;
-}
-var person1 = createPerson("hook",26,"code");
-// alert(person1.name);
-
-function xxx(name,age,job){
-	this.name = name;
-	this.age = age;
-	this.job = job;
-	this.sayName = function(){
-		alert(this.name);
-	};
-}
-// var person1 = new xxx("虾米",24,"Software Eng");
-// alert(person1.name);
-
-var m = new Map();
-m.set("Adam",67);
-m.set("Bob",59);
-m.set("Greg",90);
-for(var x of m){
-	// alert(x[0]+ '=' +x[1]);
-}
-
-
-
-
-
 var canvas = document.getElementById('myCanvas');
 var cxt = canvas.getContext("2d");
+
+cxt.fillStyle="#F0F0F0";
+cxt.fillRect(0,0,800,300);
+
+
+ //弦
+ for(var i=0;i<6;i++){
+   cxt.moveTo(0,i*30+20);
+   cxt.lineTo(800,i*30+20);
+ }
+//品
+ for(var i=0;i<4;i++){
+  cxt.moveTo(i*100+100,0);
+  cxt.lineTo(i*100+100,250);	
+ }
+  
+
+cxt.stroke();
+
+
+
 
 function makeArc(x,y,r,d){
 cxt.fillStyle = "#ff0000";
@@ -140,8 +32,56 @@ cxt.closePath();
 cxt.fill();
 }
 
-makeArc(60,18,20,0);
-makeArc(120,18,20,0);
+function makefont(notes,y,r){
+cxt.fillStyle = "green";
+cxt.font="10px console";
+cxt.strokeText(notes,y,r);
+// cxt.fill();
+}
 
-cxt.font="40px console";
-cxt.fillText("Hello World",10,50);
+function notes(arcAxis,strAxis){
+makeArc(arcAxis.x,arcAxis.y,arcAxis.r,arcAxis.d);
+makefont(strAxis.str,strAxis.x,strAxis.y);
+}
+
+var GarcAxis =  {
+    x: 50,
+    y: 18,
+    r: 10,
+    d: 0
+}
+var GstrAxis = {
+	str:"G",
+	x:47,
+	y:25
+}
+
+var AarcAxis =  {
+    x: 250,
+    y: 18,
+    r: 10,
+    d: 0
+}
+
+var AstrAxis = {
+	str:"A",
+	x:247,
+	y:25
+}
+
+var BarcAxis =  {
+    x: 150,
+    y: 48,
+    r: 10,
+    d: 0
+}
+var BstrAxis = {
+	str:"B",
+	x:147,
+	y:55
+}
+
+notes(GarcAxis,GstrAxis);
+notes(AarcAxis,AstrAxis);
+notes(BarcAxis,BstrAxis);
+
